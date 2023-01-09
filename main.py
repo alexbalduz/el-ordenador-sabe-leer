922bd45f492cbffcebc203465d9d95ecd9032402
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2023-01-09 18:00:55 +0100

    unimos en un main

---
+++ main.py
@@ -0,0 +1,8 @@
+import files.lectura_etapa_1 as etapa_1
+import files.lectura_etapa_2 as etapa_2
+import files.lectura_etapa_3 as etapa_3
+
+if __name__ == '__main__':
+    lectura_etapa_1.main()
+    lectura_etapa_2.main()
+    lectura_etapa_3.main()
